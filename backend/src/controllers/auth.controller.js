3a429e1d51ceafa98003fb25350c7c90251c0ee7
JasPanaich <jkpanaich@gmail.com>
2026-09-13 16:00:54 -0700

    Welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 // Controller to hold logic for API routes
+import { sendWelcomeEmail } from "../emails/emailHandlers.js"; // Import the sendWelcomeEmail function
+import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import { generateToken } from "../lib/utils.js";
+import { ENV } from "../lib/env.js"; 
+
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
@@ -50,8 +53,12 @@ export const signup = async (req, res) => {
             profilePic: newUser.profilePic,
         });
 
-        // TODO: Send a welcome email to the user after successful signup (optional)
-        
+        try {
+            // Send welcome email to the new user
+            await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+        } catch (error) {
+            console.error("Failed to send welcome email:", error);
+        }
     } else {
         res.status(400).json({ message: "Invalid user data" });
     }
